cbaeb99d69a38132dbad2c0ce6c30f38738218bc
Daniel Thwaites <danthwaites30@btinternet.com>
2021-08-06 02:06:04 +0100

    Actually build each fragment :sparkles:

--- src/flakeaway.js
+++ src/flakeaway.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const async = require('async')
+const util = require('util')
+const execFile = util.promisify(require('child_process').execFile)
 const { v4: uuidv4 } = require('uuid')
 const { Octokit } = require("@octokit/rest")
 const { App, createNodeMiddleware } = require('@octokit/app')
@@ -23,11 +25,17 @@ app.octokit.request('/app')
   .then(({ data }) => console.log('authenticated as %s', data.name))
 
 const queue = async.queue(
-  (task, completed) => completed(runEvaluation(task)),
+  (task, completed) => {
+    if (task.type == 'evaluation') {
+      completed(runEvaluation(task))
+    } else {
+      completed(runBuild(task))
+    }
+  },
   1
 )
 
-async function createCheck({ octokit, payload }) {
+async function createEvaluation({ octokit, payload }) {
   const owner = payload.repository.owner.login
   const repo = payload.repository.name
   const head_sha = payload.check_suite
@@ -37,16 +45,14 @@ async function createCheck({ octokit, payload }) {
   const id = uuidv4()
 
   const check_run = await octokit.rest.checks.create({
-    owner,
-    repo,
-    head_sha,
+    owner, repo, head_sha,
     external_id: id,
-    name: 'Evaluate Flake',
+    name: 'Evaluate',
     status: 'queued',
   })
 
   queue.push({
-    type: 'evaulation',
+    type: 'evaluation',
     id,
     check_run_id: check_run.data.id,
     repository: payload.repository,
@@ -57,7 +63,32 @@ async function createCheck({ octokit, payload }) {
   console.log(`Created evaluation ${id} for ${owner}/${repo}`)
 }
 
-async function readFlakeGithub(octokit, repository, revision) {
+async function createBuild({ octokit, repository, head_sha, fragment }) {
+  const id = uuidv4()
+
+  const check_run = await octokit.rest.checks.create({
+    owner: repository.owner.login,
+    repo: repository.name,
+    head_sha,
+    external_id: id,
+    name: `Build ${fragment}`,
+    status: 'queued',
+  })
+
+  queue.push({
+    type: 'build',
+    id,
+    check_run_id: check_run.data.id,
+    repository,
+    head_sha,
+    fragment,
+    octokit,
+  })
+
+  console.log(`Created build ${id}`)
+}
+
+async function flakeUrl(octokit, repository, revision) {
   const { token } = await octokit.auth({
     type: 'installation',
     repositoryIds: [repository.id],
@@ -66,7 +97,11 @@ async function readFlakeGithub(octokit, repository, revision) {
   const owner = repository.owner.login
   const repo = repository.name
 
-	const url = `git+https://oauth2:${token}@github.com/${owner}/${repo}.git?rev=${revision}`
+	return `git+https://oauth2:${token}@github.com/${owner}/${repo}.git?rev=${revision}`
+}
+
+async function readFlakeGithub(octokit, repository, revision) {
+  const url = await flakeUrl(octokit, repository, revision)
   return await readFlake(url)
 }
 
@@ -103,13 +138,53 @@ async function runEvaluation({ id, check_run_id, repository, head_sha, octokit }
     conclusion: 'success',
   })
 
+  await Promise.all(fragments.map(
+    fragment => createBuild({ octokit, repository, head_sha, fragment })
+  ))
+
   console.log(`Finished evaluation ${id}`)
 }
 
-app.webhooks.on('check_suite.requested', createCheck)
-app.webhooks.on('check_suite.rerequested', createCheck)
+async function runBuild({ id, check_run_id, repository, head_sha, fragment, octokit }) {
+  console.log(`Running build ${id}`)
+
+  const owner = repository.owner.login
+  const repo = repository.name
+
+  await octokit.rest.checks.update({
+    owner, repo, check_run_id,
+    status: 'in_progress',
+  })
+
+  const url = await flakeUrl(octokit, repository, head_sha)
+  try {
+    await execFile("nix", ["build", `${url}#${fragment}`, "--no-link"])
+  } catch (error) {
+    console.warn(error)
+    console.warn(`Failed to build ${id}`)
+    await octokit.rest.checks.update({
+      owner, repo, check_run_id,
+      status: 'completed',
+      conclusion: 'failure',
+    })
+    return
+  }
+
+  await octokit.rest.checks.update({
+    owner, repo, check_run_id,
+    status: 'completed',
+    conclusion: 'success',
+  })
+
+  console.log(`Finished build ${id}`)
+}
+
+app.webhooks.on('check_suite.requested', createEvaluation)
+app.webhooks.on('check_suite.rerequested', createEvaluation)
 app.webhooks.on('check_run.rerequested', async ({ octokit, payload }) => {
   if (payload.check_run.app.id != process.env.APP_ID) return
+
+  // TODO: Recreate individual builds
   await createCheck({ octokit, payload })
 })
 
