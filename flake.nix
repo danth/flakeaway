a2dc4714ca4740cb4c392adb563d4d5d6ff16c26
Daniel Thwaites <danthwaites30@btinternet.com>
2021-10-31 18:52:21 +0000

    Update dream2nix :arrow_up: :hammer:
    Includes changes from DavHau/dream2nix#39

--- flake.nix
+++ flake.nix
@@ -2,16 +2,9 @@
   inputs.dream2nix.url = "github:DavHau/dream2nix";
 
   outputs = { dream2nix, ... }:
-    let
-      dreamOutputs = dream2nix.lib.dream2nix.riseAndShine {
-        systems = [ "x86_64-linux" "x86_64-darwin" ];
-        dreamLock = ./dream.lock;
-        sourceOverrides = oldSources: { "flakeaway#0.0.0" = ./.; };
-      };
-    in rec {
-      inherit (dreamOutputs) defaultPackage;
-      packages = builtins.mapAttrs
-        (system: package: { flakeaway = package; })
-        defaultPackage;
+    dream2nix.lib.riseAndShine {
+      systems = [ "x86_64-linux" "x86_64-darwin" ];
+      dreamLock = ./dream.lock;
+      sourceOverrides = oldSources: { "flakeaway#0.0.0" = ./.; };
     };
 }
