16131130b516376813c93ff0fd2497d2b5a7c437
Daniel Thwaites <danthwaites30@btinternet.com>
2022-01-08 13:46:48 +0000

    Update dream2nix and Node packages :arrow_up:

--- flake.nix
+++ flake.nix
@@ -4,8 +4,7 @@
   outputs = { self, dream2nix, ... }:
     dream2nix.lib.riseAndShine {
       systems = [ "x86_64-linux" "x86_64-darwin" ];
-      dreamLock = ./dream.lock;
-      sourceOverrides = oldSources: { "flakeaway#0.0.0" = ./.; };
+      source = ./.;
     } // {
       nixosModules.flakeaway = import ./nixos-module.nix self;
     };
