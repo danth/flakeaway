771c267bec300dd635b01bbc44fec45ea72cc2b9
Daniel Thwaites <danthwaites30@btinternet.com>
2022-04-09 19:58:54 +0100

    Update dream2nix and dream-lock.json :arrow_up:

--- flake.nix
+++ flake.nix
@@ -1,11 +1,16 @@
 {
   inputs.dream2nix.url = "github:DavHau/dream2nix";
 
-  outputs = { self, dream2nix, ... }:
-    dream2nix.lib.riseAndShine {
-      systems = [ "x86_64-linux" "x86_64-darwin" ];
+  outputs =
+    inputs:
+    let
+      dream2nix =  inputs.dream2nix.lib.init {
+        systems = [ "x86_64-linux" "x86_64-darwin" ];
+        config.projectRoot = ./.;
+      };
+    in dream2nix.makeFlakeOutputs {
       source = ./.;
     } // {
-      nixosModules.flakeaway = import ./nixos-module.nix self;
+      nixosModules.flakeaway = import ./nixos-module.nix inputs.self;
     };
 }
