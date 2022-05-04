bc0a503a04c8a86e5ce17da39844d9b57c7cb92f
Daniel Thwaites <danthwaites30@btinternet.com>
2022-05-04 19:16:05 +0100

    Use `nix flake show --json` to list outputs :recycle:

--- src/nix/flake.js
+++ src/nix/flake.js
@@ -1,62 +1,8 @@
-import util from 'util'
 import _ from 'lodash'
 import { runNix } from './nix.js'
 
-function removeEscapes(text) {
-	return text
-		.replace(/\x1b\[0m/g, '')
-		.replace(/\x1b\[1m/g, '')
-		.replace(/\x1b\[32;1m/g, '')
-		.replace(/\x1b\[33;1m/g, '')
-}
-
-function blankBranches(line) {
-	return line.replace(/[│├─└]/g, ' ')
-}
-
-function removeBranches(line) {
-	return blankBranches(line).trim()
-}
-
-function removeDescription(line) {
-	return line.replace(/: .*/, '')
-}
-
-function parseLine(line) {
-	return {
-		indentation: blankBranches(line).search(/\S|$/) / 4,
-		name: removeDescription(removeBranches(line)),
-	}
-}
-
-function buildTree(lines, depth) {
-	const groups = []
-	let group = []
-	for (let i = 1; i < lines.length; i++) {
-		const line = lines[i]
-		if (line.indentation == depth) {
-			if (group.length) groups.push(group)
-			group = [line]
-		} else {
-			group.push(line)
-		}
-	}
-	if (group.length) groups.push(group)
-
-	return {
-		name: lines[0].name,
-		children: groups.map(group => buildTree(group, depth + 1)),
-	}
-}
-
-function parse(text) {
-	const lines = removeEscapes(text).split('\n')
-	const parsedLines = lines.map(parseLine).filter(line => line.name.length)
-	return buildTree(parsedLines, 1)
-}
-
 export async function readFlake(url) {
-	const { exitCode, stdout } = await runNix(["flake", "show", url])
+	const { exitCode, stdout } = await runNix(['flake', 'show', url, '--json'])
 	if (exitCode == 0) {
 		return parse(stdout)
 	} else {
@@ -64,33 +10,30 @@ export async function readFlake(url) {
 	}
 }
 
-function findChild(tree, name) {
-	return _.find(tree.children, child => child.name == name)
-}
-
+// Convert the output of `nix flake show --json` into a list of
+// strings like 'packages.x86_64-linux.example'.
 function getPaths(tree) {
-	if (tree.children.length) {
-		return _.flatMap(
-			tree.children,
-			child => getPaths(child).map(path => tree.name + '.' + path)
-		)
-	} else {
-		return [tree.name]
-	}
+	return _.flatMap(
+		Object.entries(tree),
+		([key, subtree]) => {
+			if (subtree.hasOwnProperty('type')) {
+				return key
+			} else {
+				return getPaths(subtree).map(path => `${key}.${path}`)
+			}
+		}
+	)
 }
 
-export function getBuildableFragments(tree) {
-	const fragments = []
-
-	for (let outputName of ["checks", "packages"]) {
-		const output = findChild(tree, outputName)
-		if (output) getPaths(output).forEach(path => fragments.push(path))
-	}
-
-	const nixosConfigurations = findChild(tree, "nixosConfigurations")
-	if (nixosConfigurations) getPaths(nixosConfigurations).forEach(
-		path => fragments.push(`${path}.config.system.build.toplevel`)
-	)
-
-	return fragments
+export function getBuildableFragments(flake) {
+	return getPaths(flake)
+		.map(path => {
+			if (path.startsWith('nixosConfigurations')) {
+				return `${path}.config.system.build.toplevel`
+			}
+			if (path.startsWith('checks') || path.startsWith('packages')) {
+				return path
+			}
+		})
+		.filter(path => path != null)
 }
