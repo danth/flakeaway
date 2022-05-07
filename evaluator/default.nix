3714b47e34be3e132d7c13fad6224e83354239c0
Daniel Thwaites <danthwaites30@btinternet.com>
2022-05-07 12:14:56 +0100

    Correct the license metadata for flakeaway-evaluator :page_facing_up:

--- evaluator/default.nix
+++ evaluator/default.nix
@@ -1,4 +1,4 @@
-{ boost, cmake, meson, ninja, nix, nlohmann_json, pkg-config, stdenv }:
+{ lib, boost, cmake, meson, ninja, nix, nlohmann_json, pkg-config, stdenv }:
 
 stdenv.mkDerivation rec {
   name = "flakeaway-evaluator";
@@ -13,4 +13,12 @@ stdenv.mkDerivation rec {
     # nlohmann_json can be only discovered via cmake files
     cmake
   ];
+
+  meta = {
+    description = "Parallel job evaluator for Flakeaway, based upon nix-eval-jobs";
+    homepage = "https://github.com/danth/flakeaway/tree/master/evaluator";
+    license = lib.licenses.gpl3;
+    maintainers = with lib.maintainers; [ danth ];
+    platforms = lib.platforms.unix;
+  };
 }
