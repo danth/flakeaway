4f250674bee7cabbacc465dae7bbedf6f7c7d715
Daniel Thwaites <danthwaites30@btinternet.com>
2022-08-25 00:44:34 +0100

    Remove dependency on `markdown-table` :coffin:

--- server/src/jobs/build.js
+++ server/src/jobs/build.js
@@ -1,4 +1,3 @@
-import { markdownTable } from 'markdown-table'
 import { v4 as uuidv4 } from 'uuid'
 import { isSystemError } from '../log.js'
 import { buildFragment, storeFragment, getSupportedSystems } from '../nix/build.js'
